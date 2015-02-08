f3579abb3e201f381783a8187ca5c503f3e760eb
Ivan <AlcatrazUa521@gmail.com>
2015-02-08 19:40:57 +0200

    add for table brands add,delete,update

--- SaleOfCars.loc/module/Application/Module.php
+++ SaleOfCars.loc/module/Application/Module.php
@@ -11,7 +11,10 @@ namespace Application;
 
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
-
+use Application\Model\Brands;
+use Application\Model\BrandsTable;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
 class Module
 {
     public function onBootstrap(MvcEvent $e)
@@ -36,4 +39,23 @@ class Module
             ),
         );
     }
-}
+
+    public function getServiceConfig()
+    {
+        return array(
+            'factories' => array(
+                'Application\Model\BrandsTable' =>  function($sm) {
+                    $tableGateway = $sm->get('UsersTableGateway');
+                    $table = new BrandsTable($tableGateway);
+                    return $table;
+                },
+                'UsersTableGateway' => function ($sm) {
+                    $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+                    $resultSetPrototype = new ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype(new Brands());
+                    return new TableGateway('brands', $dbAdapter, null, $resultSetPrototype);
+                },
+            ),
+        );
+    }
+    }
--- SaleOfCars.loc/module/Application/config/module.config.php
+++ SaleOfCars.loc/module/Application/config/module.config.php
@@ -20,10 +20,27 @@ return array(
                     ),
                 ),
             ),
+            'brands' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '/brands[/:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Application\Controller\Brands',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+
+
             // The following is a route to simplify getting started creating
             // new controllers and actions without needing to create a new
             // module. Simply drop new controllers in, and you can access them
             // using the path /application/:controller/:action
+
             'application' => array(
                 'type'    => 'Literal',
                 'options' => array(
@@ -52,6 +69,7 @@ return array(
             ),
         ),
     ),
+
     'service_manager' => array(
         'abstract_factories' => array(
             'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
@@ -73,7 +91,8 @@ return array(
     ),
     'controllers' => array(
         'invokables' => array(
-            'Application\Controller\Index' => 'Application\Controller\IndexController'
+            'Application\Controller\Index' => 'Application\Controller\IndexController',
+            'Application\Controller\Brands' => 'Application\Controller\BrandsController'
         ),
     ),
     'view_manager' => array(
@@ -85,6 +104,7 @@ return array(
         'template_map' => array(
             'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
             'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+            'application/brands/index' => __DIR__ . '/../view/application/brands/index.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
         ),
